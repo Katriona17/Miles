010550fba383a216593ef6cdc0acb54c46397042
Ekaterina <92870644+Katriona17@users.noreply.github.com>
2022-03-17 18:19:34 +0500

    Files added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 1055;
+        int rubles = 20;
+
+        int miles = cost / rubles;
+
+        System.out.println(miles);
+    }
+}
